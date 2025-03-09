d807309e81f74e636f2cb9f62738f1acb66da233
Ilyas Qalandarzoda <ilyas_kalandar@outlook.com>
2025-03-09 19:47:58 +0500

    feat: Add `infra` pkg.

---
+++ src/increment/infra/adapters/repos/increment.py
@@ -0,0 +1,22 @@
+from sqlalchemy import select, update
+
+from increment.domain.repos.increment import IncrementRepo
+from increment.infra.adapters.repos.base import BaseRepo
+from increment.infra.db.models import Increment
+
+
+class IncrementRepository(IncrementRepo, BaseRepo):
+
+    async def get_count(self) -> int:
+        res = await self._session.execute(select(Increment))
+
+        incr_obj = res.scalars().first()
+
+        return incr_obj.count
+
+    async def add_one(self):
+        await self._session.execute(
+            update(Increment).values(count=Increment.count + 1),
+        )
+
+        await self._session.commit()
