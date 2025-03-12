ac0a49e8e564d4c43d7f6bf004000bf086dc94ca
Ilyas Qalandarzoda <ilyas_kalandar@outlook.com>
2025-03-12 10:37:24 +0500

    feat: Add `IncrementService` implementation.

---
+++ src/increment/infra/adapters/services/increment.py
@@ -0,0 +1,22 @@
+from increment.domain.models.increment import IncrementsCount
+from increment.domain.repos.increment import IncrementRepo
+from increment.domain.services.increment import (
+    IncrementService as AbstractIncrementService,
+)
+
+
+class IncrementService(AbstractIncrementService):
+    """Adapter for `AbstractIncrementService`"""
+
+    def __init__(
+        self,
+        increment_repo: IncrementRepo,
+    ) -> None:
+        self._increment_repo = increment_repo
+
+    async def get_count(self) -> IncrementsCount:
+        count = await self._increment_repo.get_count()
+        return count
+
+    async def increment(self) -> None:
+        await self._increment_repo.add_one()
