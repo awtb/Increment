ba450fefc47ffd8772c20826973b2e2332aa2930
Ilyas Qalandarzoda <ilyas_kalandar@outlook.com>
2025-03-12 10:36:40 +0500

    feat: Returning `IncrementsCount` following `IncrementRepo` contract.

--- src/increment/infra/adapters/repos/increment.py
+++ src/increment/infra/adapters/repos/increment.py
@@ -1,18 +1,18 @@
 from sqlalchemy import select, update
 
+from increment.domain.models.increment import IncrementsCount
 from increment.domain.repos.increment import IncrementRepo
 from increment.infra.adapters.repos.base import BaseRepo
 from increment.infra.db.models import Increment
 
 
 class IncrementRepository(IncrementRepo, BaseRepo):
-
-    async def get_count(self) -> int:
+    async def get_count(self) -> IncrementsCount:
         res = await self._session.execute(select(Increment))
 
         incr_obj = res.scalars().first()
 
-        return incr_obj.count
+        return IncrementsCount(count=incr_obj.count)  # type: ignore
 
     async def add_one(self):
         await self._session.execute(
