8658fabe8f408786b436da9aa23e970c9fe22272
Ilyas Qalandarzoda <ilyas.qalandarzoda@icloud.com>
2025-04-05 15:18:05 +0500

    fix: `V1Controller`

--- src/increment/api/controllers/increment_v1.py
+++ src/increment/api/controllers/increment_v1.py
@@ -1,16 +1,32 @@
-from blacksheep.server.controllers import APIController, post
+from blacksheep.server.controllers import APIController, get, post
+
+from increment.domain.models.increment import IncrementsCount
+from increment.domain.services.increment import IncrementService
 
 
 class IncrementController(APIController):
     @classmethod
     def version(cls) -> str:
-        return "v2"
+        return "v1"
 
     @classmethod
     def class_name(cls) -> str:
         return "increment"
 
     @post()
-    async def increment(self):
-        """Fast version of Increment API, v2"""
-        raise NotImplementedError()
+    async def make_increment(self, increment_service: IncrementService):
+        """
+        Slower version of increment, v1
+        """
+
+        await increment_service.increment()
+
+    @get()
+    async def get_increments_count(
+        self,
+        service: IncrementService,
+    ) -> IncrementsCount:
+        """Get count of increments"""
+        count = await service.get_count()
+
+        return count
