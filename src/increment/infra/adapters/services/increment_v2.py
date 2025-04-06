54b388e34124282c38b56bd7b5cc57b0d1f1251c
Ilyas Qalandarzoda <ilyas.qalandarzoda@icloud.com>
2025-04-06 18:43:54 +0500

    fix: Contract.

--- src/increment/infra/adapters/services/increment_v2.py
+++ src/increment/infra/adapters/services/increment_v2.py
@@ -1,10 +1,10 @@
 from increment.domain.models.increment import IncrementsCount
-from increment.domain.repos.increment import IncrementRepo
+from increment.domain.repos.increment_v2 import IncrementV2Repository
 from increment.domain.services.increment_v2 import IncrementV2Service
 
 
 class IncrementServiceV2Adapter(IncrementV2Service):
-    def __init__(self, increment_repo: IncrementRepo):
+    def __init__(self, increment_repo: IncrementV2Repository):
         super().__init__()
         self._increment_repo = increment_repo
 
