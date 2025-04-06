48252be962ae4329fe3915311256367004cb3e3e
Ilyas Qalandarzoda <ilyas.qalandarzoda@icloud.com>
2025-04-06 18:44:41 +0500

    feat: Setting up counter before startup.

--- src/increment/api/main.py
+++ src/increment/api/main.py
@@ -2,6 +2,7 @@
 This module configures the BlackSheep application before it starts.
 """
 
+import logging
 from typing import Type
 
 from blacksheep import Application
@@ -19,6 +20,7 @@ from increment.api.docs import configure_docs
 from increment.api.errors import configure_error_handlers
 from increment.api.services import configure_services
 from increment.api.settings import Settings, load_settings
+from increment.domain.models.increment import IncrementsCount
 from increment.domain.repos.increment import IncrementRepo
 from increment.domain.repos.increment_v2 import IncrementV2Repository
 from increment.domain.services.increment import IncrementService
@@ -47,6 +49,7 @@ def session_factory(
 async def configure_sqlalchemy_engine(
     application: Application,
 ):
+    logging.info("Configuring SQLAlchemy engine")
     settings = application.services.resolve(Settings)
 
     url = URL.create(
@@ -82,6 +85,11 @@ async def configure_sqlalchemy_engine(
     application.services.add_scoped(IncrementRepo, IncrementRepository)
     application.services.add_scoped(IncrementService, IncrementAdapter)
 
+    srv = application.services.resolve(IncrementService)
+    incr_count = await srv.get_count()
+
+    application.services.add_instance(incr_count)
+
     # V2
     application.services.add_scoped(
         IncrementV2Service,
@@ -93,18 +101,38 @@ async def configure_sqlalchemy_engine(
     )
 
 
+async def configure_counter(
+    application: Application,
+):
+    logging.info("Configuring global counter for V2 service.")
+    srv = application.services.resolve(IncrementService)
+    incr_count = await srv.get_count()
+
+    application.services.register(IncrementsCount, incr_count)
+
+
 async def dispose_sqlalchemy_engine(
     application: Application,
 ):
+    logging.info("Disposing connection to SQLAlchemy engine")
     engine = application.services.resolve(AsyncEngine)
 
     await engine.dispose()
+
+
+async def flush_counter(
+    application: Application,
+):
+    logging.debug("Flushing increments counter")
+    repo = application.services.resolve(IncrementV2Repository)
+    await repo.flush_counter()
 
 
 def configure_application(
     services: Container,
     settings: Settings,
 ) -> Application:
+    logging.basicConfig(level=settings.log_level)
     app = Application(
         services=services, show_error_details=settings.app.show_error_details
     )
@@ -114,6 +142,8 @@ def configure_application(
     configure_docs(app, settings)
 
     app.on_start += configure_sqlalchemy_engine
+
+    app.on_stop += flush_counter
     app.on_stop += dispose_sqlalchemy_engine
 
     return app
