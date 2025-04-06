8f55e1899adfb83c0e4e62b5ba618ec183b6ff35
Ilyas Qalandarzoda <ilyas.qalandarzoda@icloud.com>
2025-04-06 18:43:14 +0500

    feat: Add `flush_counter` method to contract.p

--- src/increment/domain/repos/increment_v2.py
+++ src/increment/domain/repos/increment_v2.py
@@ -5,11 +5,15 @@ from increment.domain.models.increment import IncrementsCount
 
 class IncrementV2Repository(ABC):
     @abstractmethod
-    async def increment(self) -> None:
+    async def add_one(self) -> None:
         """Increments inner count by 1"""
         raise NotImplementedError
 
     @abstractmethod
     async def get_count(self) -> IncrementsCount:
         """Increments inner count by 1"""
+        raise NotImplementedError
+
+    @abstractmethod
+    async def flush_counter(self) -> None:
         raise NotImplementedError
