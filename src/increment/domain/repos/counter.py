4707ca9618b71190268d4fadef97213f1905c106
Ilyas Qalandarzoda <ilyas_kalandar@outlook.com>
2025-08-23 19:28:03 +0500

    refactor(counter-repo): Move flushing to periodic task.

--- src/increment/domain/repos/counter.py
+++ src/increment/domain/repos/counter.py
@@ -14,29 +14,23 @@ class CounterRepo(BaseRepo):
         self,
         settings: Settings,
         session: AsyncSession,
-        increments_count: Counter,
+        global_counter: Counter,
     ) -> None:
         super().__init__(session)
-        self._counter = increments_count
+        self._counter = global_counter
         self._update_interval = settings.counter_flush_interval
         self._logger = logging.getLogger(
             f"{__name__}.{self.__class__.__name__}",
         )
 
     async def flush_counter(self) -> None:
-        self._logger.info("Flushing increments counter")
         await self._session.execute(
             update(Increment).values(count=self._counter.count),
         )
         await self._session.commit()
 
     async def add_one(self) -> None:
-        self._logger.debug(f"IncrementV2RepoAdapter.add_one: {self._counter}")
-
         self._counter.count += 1
-
-        if self._counter.count % self._update_interval == 0:
-            await self.flush_counter()
 
     async def get_counter(self) -> Counter:
         return self._counter
